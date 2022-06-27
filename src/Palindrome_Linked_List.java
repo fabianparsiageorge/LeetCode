9015e6e87de55d66fd04452cf2bbf5bb268f8f25
Fabian Parsia George <fabian.parsia.george@g.bracu.ac.bd>
2022-06-27 18:41:41 +0600

    LeetCode Problems Solved
    Signed-off-by: Fabian Parsia George <fabian.parsia.george@g.bracu.ac.bd>

--- src/Palindrome_Linked_List.java
+++ src/Palindrome_Linked_List.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 class Palindrome_Linked_List {
     public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(2, new ListNode(1, null)))));
+        Boolean result = isPalindrome(head);
+        System.out.println(result);
+    }
 
+    public static boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (head != null) {
+            arr.add(head.val);
+            head = head.next;
+        }
+        int i = 0, j = arr.size() - 1;
+        while (i < j) {
+            if (arr.get(i) != arr.get(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
