36302a90e88f24d8862c1666bfd76e1ee1690968
Fabian Parsia George <fabian.parsia.george@g.bracu.ac.bd>
2022-07-02 02:16:59 +0600

    LeetCode Problems Solved
    Signed-off-by: Fabian Parsia George <fabian.parsia.george@g.bracu.ac.bd>

--- src/Longest_Substring.java
+++ src/Longest_Substring.java
@@ -1,29 +1,27 @@
 public class Longest_Substring {
     public static void main(String[] args) {
-        String s = "abcabcbb";
+        String s = "dvdf";
         int substringLength = lengthOfLongestSubstring(s);
         System.out.println(substringLength);
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        String substring = "";
-        for (int i = 0; i < s.length(); i++) {
-            String letter = String.valueOf(s.charAt(i));
-            if (substring.contains(letter)) {
-                System.out.println(letter);
-                System.out.println(letter + " is in " + substring);
-                String rgx = "^[a-z]?" + letter;
-                substring = substring.replaceAll(rgx, "");
-                substring += letter;
-                System.out.println(substring);
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
 
+        String substring = "";
+
+        while (j < s.length()) {
+            if (!substring.contains(String.valueOf(s.charAt(j)))) {
+                substring += String.valueOf(s.charAt(j));
+                maxLength = Math.max(substring.length(), maxLength);
+                j++;
             } else {
-                System.out.println(letter);
-                System.out.println(letter + " is not in " + substring);
-                substring += letter;
-                System.out.println(substring);
+                substring = substring.replace(String.valueOf(s.charAt(i)), "");
+                i++;
             }
         }
-        return substring.length();
+        return maxLength;
     }
 }
