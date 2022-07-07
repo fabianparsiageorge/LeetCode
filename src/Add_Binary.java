005717ede83236e247e716fc1b9a7e3dde7be1f8
Fabian Parsia George <fabian.parsia.george@g.bracu.ac.bd>
2022-07-08 04:22:07 +0600

    LeetCode Problems Solved

---
+++ src/Add_Binary.java
@@ -0,0 +1,5 @@
+public class Add_Binary {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Plus_One.java
+++ src/Plus_One.java
@@ -1,5 +1,3 @@
-import java.math.BigInteger;
-
 public class Plus_One {
     public static void main(String[] args) {
         int[] digits = {7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6};
@@ -11,36 +9,16 @@ public class Plus_One {
     }
 
     public static int[] plusOne(int[] digits) {
-        String str = "";
-        boolean allNine = false;
-        int[] result;
-
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] == 9) {
-                allNine = true;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             } else {
-                allNine = false;
-                break;
+                digits[i] = 0;
             }
         }
-        if (allNine) {
-            result = new int[digits.length + 1];
-        } else {
-            result = new int[digits.length];
-        }
-
-        for (int i : digits) {
-            str += i;
-        }
-        BigInteger b = new BigInteger(str);
-        BigInteger c = new BigInteger("1");
-        b = b.add(c);
-        str = "" + b;
-
-        for (int i = 0; i <= str.length() - 1; i++) {
-            String s = String.valueOf(str.charAt(i));
-            result[i] = Integer.parseInt(s);
-        }
-        return result;
+        int[] arr = new int[digits.length + 1];
+        arr[0] = 1;
+        return arr;
     }
 }
