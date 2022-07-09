40fa4b5022b169ce3de29e8f14d30e7469530e95
Fabian Parsia George <fabian.parsia.george@g.bracu.ac.bd>
2022-07-09 22:16:05 +0600

    LeetCode Problems Solved

--- src/Add_Binary.java
+++ src/Add_Binary.java
@@ -1,5 +1,33 @@
 public class Add_Binary {
     public static void main(String[] args) {
+        String a = "1010", b = "1011";
+        String result = addBinary(a, b);
+        System.out.println(result);
+    }
 
+    public static String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        StringBuilder ouput = new StringBuilder();
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+            }
+            ouput.append(sum % 2);
+            carry = sum / 2;
+            i--;
+            j--;
+        }
+
+        if (carry == 1) {
+            ouput.append(carry);
+        }
+        return ouput.reverse().toString();
     }
 }
